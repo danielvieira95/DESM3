0aa3b31c53c56c5d09bd89cd8cfe6c6abdf30154
danielvieira95 <engdanielvieira95@gmail.com>
2024-08-20 15:20:50 -0300

    single scroll view

--- Turma_A/Aula03/appcardapiorestaurante/lib/screens/Highlight.dart
+++ Turma_A/Aula03/appcardapiorestaurante/lib/screens/Highlight.dart
@@ -1,6 +1,7 @@
 import 'package:appcardapiorestaurante/cardapio.dart';
 import 'package:appcardapiorestaurante/component/Highlights_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Highlight extends StatelessWidget {
   final List items = destaques;
@@ -9,14 +10,30 @@ class Highlight extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(padding: EdgeInsets.all(16),
-    child: ListView.builder(
-      itemBuilder: (context,index){
-        return Highlight_items(img:items[index]["image"], 
-        nome:items[index]["name"],price:items[index]["price"], 
-        description:items[index]["description"]);
-      },
-      itemCount: items.length,
-      ),
+    child:CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: Text("Destaques")),
+        SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+          return Highlight_items(
+                img: items[index]["image"],
+                nome: items[index]["name"],
+                price: items[index]["price"],
+                description: items[index]["description"]);
+            
+          
+        },
+        childCount: items.length,
+        ),
+       
+        ),
+        
+       
+      ],
+    ),  
+    
+    
+    
+     
       
       );
   }
