bebbe7a20739d13908bff7937706f1f108f3c9f2
danielvieira95 <engdanielvieira95@gmail.com>
2024-08-20 22:24:29 -0300

    componente order item

---
+++ Turma_B/Aula03/app_restaurante/lib/components/Orderitem.dart
@@ -0,0 +1,51 @@
+import 'package:app_restaurante/components/Counter.dart';
+import 'package:flutter/material.dart';
+
+class OrderItem extends StatefulWidget {
+  const OrderItem(
+      {Key? key,
+      required this.imageURI,
+      required this.itemTitle,
+      required this.itemPrice})
+      : super(key: key);
+  final String imageURI;
+  final String itemTitle;
+  final String itemPrice;
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      clipBehavior: Clip.hardEdge,
+      color: Theme.of(context).colorScheme.surfaceVariant,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image(
+            image: AssetImage(widget.imageURI),
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(widget.itemTitle),
+                Text(widget.itemPrice),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: CounterComponente(),
+          ),
+        ],
+      ),
+    );
+  }
+}
