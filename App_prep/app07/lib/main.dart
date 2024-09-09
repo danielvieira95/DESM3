45ba45c349833cf2196badd739643aa95ec09f28
danielvieira95 <engdanielvieira95@gmail.com>
2024-09-09 16:35:42 -0300

    listin firebase

--- App_prep/app07/lib/main.dart
+++ App_prep/app07/lib/main.dart
@@ -139,3 +139,21 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class Listin {
+  String id;
+  String name;
+
+  Listin({required this.id, required this.name});
+
+  Listin.fromMap(Map<String, dynamic> map)
+      : id = map["id"],
+        name = map["name"];
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+    };
+  }
+}
